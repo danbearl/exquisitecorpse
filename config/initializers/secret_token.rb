54bb220d66108793d7a3461ec4f552fb0ff7d254
Micah Cooper and Taylor Mock <dev+mrmicahcooper+tmock12@hashrocket.com>
2013-03-22 15:21:39 -0400

    Initial commit Bitch

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Exquisitecorpse::Application.config.secret_key_base = '8d72400087224e2055171c97fb515fc2cf2f08a5d863d9865e058ceb2626d559c4d6c4c4e074aca37fef46e63494c3cc10968946979d77d62c5f614db771ff2a'
