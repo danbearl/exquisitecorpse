4b4b24baf22fe2b492a10af7bd557bd4453f14b0
Dan Bearl <dan.bearl@gmail.com>
2013-08-07 12:28:47 -0400

    Add truncate_body to snippets
    - Trims the snippet's body to 300 characters before save

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -2,5 +2,12 @@ class Snippet < ActiveRecord::Base
 
   belongs_to :corpse
   belongs_to :author, class_name: 'User'
+  before_save :truncate_body
+
+  def truncate_body
+    if self.body.length > 300
+      self.body = self.body[0,300]
+    end
+  end
   
 end
