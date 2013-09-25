07a06ae855c953dca9978d7722ad2fdf91d4c50d
Dan Bearl <dan.bearl@gmail.com>
2013-09-24 21:01:23 -0400

    Add recent activitty to front page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,5 @@
 class PagesController < ApplicationController
+  expose(:recent_corpses) { Corpse.where("closed = true").limit(5) }
+  expose(:recent_snippets) { Snippet.all.order("created_at DESC").limit(5) }
+  expose(:recent_posts) { Post.all.order("created_at DESC").limit(5) }
 end
